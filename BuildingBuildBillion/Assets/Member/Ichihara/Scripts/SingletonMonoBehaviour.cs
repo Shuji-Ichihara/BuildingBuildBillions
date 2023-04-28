50f811939ce8e8913ef4143b22a4ae85fba374d1
Shuji-Ichihara <shuji.birth-0507@outlook.jp>
2023-04-28 13:10:25 +0900

    Create SingletonMonoBehaviour.cs
    シングルトンクラスの基底クラスを作成した。

---
+++ BuildingBuildBillion/Assets/Member/Ichihara/Scripts/SingletonMonoBehaviour.cs
@@ -0,0 +1,38 @@
+using System;
+using UnityEngine;
+
+public class SingletonMonoBehaviour<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T _instance;
+    public static T Instance
+    {
+        get
+        {
+            if(_instance == null)
+            {
+                Type t = typeof(T);
+                _instance = (T)FindObjectOfType(t);
+                if(_instance == null)
+                {
+                    Debug.LogError(t + "をあたっちしている GameObject はありません。");
+                }
+            }
+            return _instance;
+        }
+    }
+
+    protected virtual void Awake()
+    {
+        // 他の GameObject にアタッチされているか調べる。
+        // アタッチされている場合は破棄する。
+        if(this != Instance)
+        {
+            Destroy(this);
+            Debug.LogError(
+                typeof(T)
+                + "は既に他の GameObject にアタッチされているため、コンポーネントを破棄しました。"
+                + "アタッチされている GameObject は" + Instance.gameObject.name + "です。");
+            return;
+        }
+    }
+}
