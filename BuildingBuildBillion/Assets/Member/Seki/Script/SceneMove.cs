b55bd385db23297e6ca2d9e78c9bfd5dd1753ac0
sekiRyoma <ryo-ma0110@docomo.ne.jp>
2023-04-28 16:12:24 +0900

    scenesenni

---
+++ BuildingBuildBillion/Assets/Member/Seki/Script/SceneMove.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneMove : MonoBehaviour
+{
+    public static SceneMove instance;
+    [SerializeField]
+    private string _titleSceneName, _mainGameSceneName, _resultSceneName;
+
+    private void Awake()
+    {
+        if(instance==null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void Title()
+    {
+        SceneManager.LoadScene(_titleSceneName);
+    }
+    public void MainGame()
+    {
+        SceneManager.LoadScene(_mainGameSceneName);
+    }
+    public void Result()
+    {
+        SceneManager.LoadScene(_resultSceneName);
+    }
+}
