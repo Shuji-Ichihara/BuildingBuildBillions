09a67b6f841f15de5ec2208a6afed7efe07288bf
Shuji-Ichihara <shuji.birth-0507@outlook.jp>
2023-05-12 10:54:35 +0900

    Update CameraControllerTest.cs
    カメラの Y 座標を移動させるテストスクリプトを作成した。

--- BuildingBuildBillion/Assets/Member/Ichihara/Scripts/CameraControllerTest.cs
+++ BuildingBuildBillion/Assets/Member/Ichihara/Scripts/CameraControllerTest.cs
@@ -17,10 +17,10 @@ public class CameraControllerTest : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //ホイールを取得して、均しのためにtime.deltaTimeをかけておく
+        // カメラズームのテストプログラム
         var scroll = Input.GetAxis("Vertical") * Time.deltaTime * 100;
         Debug.Log(_mainCamera.orthographicSize);
-        MainCameraMove(scroll);
+        MainCameraMove();
 
         // mainCam.orthographicSizeは0だとエラー出るっぽいので回避策
         if (_mainCamera.orthographicSize >= 540 && _mainCamera.orthographicSize <= 540 * 2)
@@ -33,10 +33,23 @@ public class CameraControllerTest : MonoBehaviour
 
     }
 
-    private void MainCameraMove(float mouseScrollWheel)
+    private void MainCameraMove()
     {
+        float cameraMoveYValue = 1.0f;
         // カメラの z 座標を移動
-        _mainCamera.transform.position += new Vector3(0, 0, mouseScrollWheel);
-        Debug.Log($"{_mainCamera.transform.position.z}");
+        if (Input.GetKey(KeyCode.A))
+        {
+            _mainCamera.transform.position += Vector3.up * cameraMoveYValue * Time.deltaTime * 100;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            _mainCamera.transform.position += Vector3.down * cameraMoveYValue * Time.deltaTime * 100;
+        }
+        // カメラの Y 座標の移動範囲を制限
+        _mainCamera.transform.position = new Vector3(0.0f
+                                                   , Mathf.Clamp(_mainCamera.transform.position.y
+                                                               , 0.0f
+                                                               , Screen.height / 2.0f)
+                                                   , _mainCamera.transform.position.z);
     }
 }
