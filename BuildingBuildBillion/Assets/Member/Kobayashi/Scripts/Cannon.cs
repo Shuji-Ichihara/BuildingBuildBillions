0b65dd652898bda70590c9afe5b4bacc8b62542c
tyobo <cchobo32@gmail.com>
2023-05-12 18:45:00 +0900

    大砲完成、Xboxコントローラー対応確認

--- BuildingBuildBillion/Assets/Member/Kobayashi/Scripts/Cannon.cs
+++ BuildingBuildBillion/Assets/Member/Kobayashi/Scripts/Cannon.cs
@@ -4,64 +4,74 @@ using UnityEngine;
 
 public class Cannon : MonoBehaviour
 {
-    [SerializeField, Header("大砲の回転角度")] private float _rotValue; //大砲の回転角度数
     [SerializeField, Header("発射までにかかる時間")] private float _fireTime; //発射にかかる秒数
     [SerializeField, Header("弾発射のインパクト")] private float _cannnonBulletImpact;   //弾のスピード
     [SerializeField] private GameObject _cannonBullet;
     [SerializeField] private GameObject _cannonMuzzle;
     private Transform _cannonTransform;
+    private Rigidbody2D _rg;
+    private float rotValue = 1;//仮変数
 
     private void Start()
     {
+        _rg = this.gameObject.GetComponent<Rigidbody2D>();
+        StartCoroutine(CannonFire());//こいつの呼ぶ場所は配置したらでおね。
     }
     private void Update()
     {
         _cannonTransform = this.transform;
+        Move(Input.GetAxis("D_Pad_H"), Input.GetAxis("D_Pad_V"));
+    }
+    private void Move(float hori, float ver)
+    {
         if (Input.GetKey(KeyCode.D))
         {
-            CannonRightRotation();
+            CannonRotation(-rotValue);
         }
         if (Input.GetKey(KeyCode.A))
         {
-            CannonLeftRotation();
+            CannonRotation(rotValue);
         }
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.H))
         {
-            CannonFire();
+            _rg.constraints = RigidbodyConstraints2D.None;
         }
-    }
-    /// <summary>
-    /// 大砲の右回転メソッド
-    /// </summary>
-    public void CannonLeftRotation()
-    {
 
-        // ローカル座標を基準に、回転を取得
-        Vector3 localangle = _cannonTransform.localEulerAngles;
-        //ローカル座標を基準にして_rotValue分回転
-        localangle.z += _rotValue;
-        _cannonTransform.localEulerAngles = localangle; // 回転角度を設定
+        if (Input.GetKeyDown("joystick button 4"))
+        {
+            Debug.Log("L"); //L?
+        }
+        if (Input.GetKeyDown("joystick button 5"))
+        {
+            Debug.Log("R");//R?
+        }
+        Debug.Log(hori);
+        Debug.Log(ver);
     }
     /// <summary>
-    /// 大砲の左回転メソッド
+    /// 大砲の回転メソッド
+    /// 引数:float RotValue
+    /// 右回転の時は引数にマイナスをつけてください。
     /// </summary>
-    public void CannonRightRotation()
+    public void CannonRotation(float RotValue)
     {
         // ローカル座標を基準に、回転を取得
         Vector3 localangle = _cannonTransform.localEulerAngles;
         //ローカル座標を基準にして_rotValue分回転
-        localangle.z -= _rotValue;
+        localangle.z += RotValue;
         _cannonTransform.localEulerAngles = localangle; // 回転角度を設定
     }
     /// <summary>
     /// 大砲の攻撃メソッド
     /// </summary>
-    public void CannonFire()
+    public IEnumerator CannonFire()
     {
+        _rg.constraints = RigidbodyConstraints2D.FreezeRotation;
+        yield return new WaitForSeconds(_fireTime);
         GameObject clone = Instantiate(_cannonBullet, _cannonMuzzle.transform.position, this.transform.rotation);
-
-        //Rigidbody2D rg2d = clone.GetComponent<Rigidbody2D>();
         clone.GetComponent<Rigidbody2D>().AddForce(_cannnonBulletImpact * _cannonMuzzle.transform.right, ForceMode2D.Impulse);
+        _rg.constraints = RigidbodyConstraints2D.None;
     }
+
 
 }
