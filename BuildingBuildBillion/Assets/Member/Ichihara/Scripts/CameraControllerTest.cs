86e0ed5459c421129765c3f2d5ddcf5e4533d72e
Shuji-Ichihara <shuji.birth-0507@outlook.jp>
2023-05-12 18:47:12 +0900

    Update CameraControllerTest.cs
    カメラのズームイン、ズームアウトを自動化した(途中)。

--- BuildingBuildBillion/Assets/Member/Ichihara/Scripts/CameraControllerTest.cs
+++ BuildingBuildBillion/Assets/Member/Ichihara/Scripts/CameraControllerTest.cs
@@ -1,62 +1,123 @@
-﻿using System.Collections;
+﻿using Cysharp.Threading.Tasks;
+using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class CameraControllerTest : MonoBehaviour
 {
-    // メインカメラ
+    // ズームさせるカメラ
     [SerializeField]
     private Camera _camera = null;
-    public Camera Camera => _camera;
+    // カメラズームのスピード
+    [SerializeField]
+    private float _zoomSpeed = 100.0f;
 
     // Start is called before the first frame update
-    void Start()
+    async void Start()
     {
-        //_mainCamera = Camera.main;
+        _camera.orthographicSize = 540.0f;
+        await CalucrateCameraMovement(new CancellationToken());
     }
 
     // Update is called once per frame
     void Update()
     {
-        ZoomCamera();
+        //await ZoomCamera(_token);
     }
 
     /// <summary>
     /// カメラズームのプログラム
     /// </summary>
-    private void ZoomCamera()
+    private async UniTask CalucrateCameraMovement(CancellationToken token)
     {
         // カメラの OrthographicSize の変化量
-        float zoom = Input.GetAxis("Vertical") * Time.deltaTime * 100;
+        float zoom = _zoomSpeed * Time.deltaTime;
+        // カメラの Y 座標の変化量
+        float cameraMoveYValue = 100.0f;
 
-        // mainCam.orthographicSizeは0だとエラー出るっぽいので回避策
-        if (_camera.orthographicSize >= 540 && _camera.orthographicSize <= 540 * 2)
+        /*
+        // カメラの Orthgraphic 野初期値は 540 を想定
+        if (_camera.orthographicSize >= 540 && _camera.orthographicSize <= Screen.height)
         {
-            _camera.orthographicSize += zoom;
+            // 押したボタンに応じてズームイン、ズームアウト
+            if (Input.GetKeyDown(KeyCode.I))
+            {
+                // I キーを押したらズームイン
+                if (zoom < 0.0f) { zoom *= -1; }
+
+                while (_camera.orthographicSize > 540 || _camera.orthographicSize < Screen.height)
+                {
+                    _camera.orthographicSize += zoom;
+                    _camera.transform.position += Vector3.up * cameraMoveYValue * Time.deltaTime;
+                    GameManager.Instance.MoveJadgementBarFallPoint(true);
+                    await UniTask.Yield();
+                }
+            }
+            // O キーを押したらズームアウト
+            else if (Input.GetKeyDown(KeyCode.O))
+            {
+                zoom *= -1;
+                while (_camera.orthographicSize > 540 || _camera.orthographicSize < Screen.height)
+                {
+                    _camera.orthographicSize += zoom;
+                    _camera.transform.position += Vector3.down * cameraMoveYValue * Time.deltaTime;
+                    GameManager.Instance.MoveJadgementBarFallPoint(false);
+                    await UniTask.Yield();
+                }
+            }
+            // カメラの Orthgraphic の限界値を定義
             _camera.orthographicSize = Mathf.Clamp(_camera.orthographicSize
                                                      , 540
                                                      , Screen.height);
-        }
+            // カメラの Y 座標の移動範囲を定義
+            _camera.transform.position = new Vector3(0.0f
+                                                   , Mathf.Clamp(_camera.transform.position.y
+                                                                , 0.0f
+                                                                , Screen.height / 2.0f)
+                                                   , _camera.transform.position.z);
+        }*/
 
-        // カメラの Y 座標の移動量
-        float cameraMoveYValue = 100.0f;
-        // カメラの z 座標を移動
-        if (zoom > 0.0f)
+        // カメラの Y 座標の移動方向を格納する変数
+        Vector3 dummyVector3 = Vector3.zero;
+        // 判定バーの移動方向の設定
+        bool moveCameraSwtich = false;
+        while(_camera.orthographicSize >= 540 && _camera.orthographicSize <= Screen.height)
         {
-            _camera.transform.position += Vector3.up * cameraMoveYValue * Time.deltaTime;
+            // I キーを押したらズームイン
+            if (Input.GetKeyDown(KeyCode.I))
+            {
+                if (zoom < 0.0f) { zoom *= -1; }
+                dummyVector3 = Vector3.up;
+                moveCameraSwtich = true;
+            }
+            // O キーを押したらズームアウト
+            else if (Input.GetKeyDown(KeyCode.O))
+            {
+                zoom *= -1;
+                dummyVector3 = Vector3.down;
+                moveCameraSwtich = false;
+            }
+            await MoveCamera(zoom, dummyVector3, cameraMoveYValue, moveCameraSwtich, new CancellationToken());
+            // カメラの Orthgraphic の限界値を定義
+            _camera.orthographicSize = Mathf.Clamp(_camera.orthographicSize
+                                                     , 540.0f
+                                                     , Screen.height);
         }
-        else if (zoom < 0.0f)
-        {
-            _camera.transform.position += Vector3.down * cameraMoveYValue * Time.deltaTime;
-        }
-        // カメラの Y 座標の移動範囲を制限
+    }
+
+    private async UniTask MoveCamera(float zoom, Vector3 vector, float MoveY, bool moveSwitch, CancellationToken token)
+    {
+        _camera.orthographicSize += zoom;
+        _camera.transform.position += vector * MoveY;
+        GameManager.Instance.MoveJadgementBarFallPoint(moveSwitch);
+        // カメラの Y 座標の移動範囲を定義
         _camera.transform.position = new Vector3(0.0f
                                                , Mathf.Clamp(_camera.transform.position.y
                                                             , 0.0f
                                                             , Screen.height / 2.0f)
                                                , _camera.transform.position.z);
-        GameManager.Instance.MoveJadgementBarFallPoint(zoom);
+        await UniTask.Yield();
     }
     // 自動でカメラズームを制御
-    // ズームに応じてカメラの Y 座標を移動
 }
