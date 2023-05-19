645598987642dc94dad8d43a25d08060c018770e
Shuji-Ichihara <shuji.birth-0507@outlook.jp>
2023-05-19 16:09:23 +0900

    Update JadgementBarControllerTest.cs
    player1 と player2 の勝敗判定の分岐処理を作成した。

--- BuildingBuildBillion/Assets/Member/Ichihara/Scripts/JadgementBarControllerTest.cs
+++ BuildingBuildBillion/Assets/Member/Ichihara/Scripts/JadgementBarControllerTest.cs
@@ -18,18 +18,13 @@ public class JadgementBarControllerTest : SingletonMonoBehaviour<JadgementBarCon
     // Start is called before the first frame update
     void Start()
     {
-        _jadgementBar.AddComponent<Rigidbody2D>();
+        _jadgementBar.AddComponent<JadgementBar>();
         _rb2D = _jadgementBar.GetComponent<Rigidbody2D>();
-        _rb2D.constraints = RigidbodyConstraints2D.FreezePositionX;
+        _rb2D.constraints = RigidbodyConstraints2D.FreezeRotation;
         _rb2D.gravityScale = 0.0f;
+        // 初期座標を代入
         _jadgementBarFallPosition = Vector3.up * (Screen.height / 2.0f + _jadgementBarFallPointHeight);
         transform.position = _jadgementBarFallPosition;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
     }
 
     private void FixedUpdate()
@@ -45,7 +40,7 @@ public class JadgementBarControllerTest : SingletonMonoBehaviour<JadgementBarCon
     /// <summary>
     /// カメラのズーム、移動に連動して JadgementBarFallPoint を Y 軸方向に移動させる
     /// </summary>
-    /// <param name="yAxis">カメラのズームの移動方向</param>
+    /// <param name="yAxis">カメラの Y 軸の移動方向</param>
     public void MoveJadgementBarFallPoint(bool yAxis)
     {
         if (yAxis == true)
@@ -61,5 +56,26 @@ public class JadgementBarControllerTest : SingletonMonoBehaviour<JadgementBarCon
                                                     ,_jadgementBarFallPosition.y
                                                     ,Screen.height * 1.5f + _jadgementBarFallPointHeight)
                                         ,0.0f);
+    }
+
+    /// <summary>
+    /// 勝敗判定
+    /// </summary>
+    /// <param name="vec2">接触したオブジェクトの座標</param>
+    public void Jadgement(Vector2 vec2)
+    {
+        if (vec2.x < 0.0f)
+        {
+            Debug.Log($"Player1 Win!!");
+        }
+        else if (vec2.x > 0.0f)
+        {
+            Debug.Log($"Player2 Win!!");
+        }
+        // 接触したオブジェクトの x 座標が 0 の場合(後で取り掛かる)
+        else
+        {
+            Debug.Log($"Draw");
+        }
     }
 }
