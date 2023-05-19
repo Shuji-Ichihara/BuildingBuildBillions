1f3782c00b5845b1bffcf51e68fee171b3acdfd4
Shuji-Ichihara <shuji.birth-0507@outlook.jp>
2023-05-19 14:30:15 +0900

    Create JadgementBarControllerTest.cs
    勝敗判定に関する機能を GameManager スクリプトから移管した。

---
+++ BuildingBuildBillion/Assets/Member/Ichihara/Scripts/JadgementBarControllerTest.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JadgementBarControllerTest : SingletonMonoBehaviour<JadgementBarControllerTest>
+{
+    [Header("勝敗判定")]
+    // 勝敗判定バー
+    [SerializeField]
+    private GameObject _jadgementBar = null;
+    private Rigidbody2D _rb2D = null;
+    // 画面トップから _jadgementBarFallPoint をどのくらい離すかを決める
+    [SerializeField]
+    private float _jadgementBarFallPointHeight = 0.0f;
+    // アタッチされているオブジェクトの初期座標
+    private Vector3 _jadgementBarFallPosition = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _jadgementBar.AddComponent<Rigidbody2D>();
+        _rb2D = _jadgementBar.GetComponent<Rigidbody2D>();
+        _rb2D.constraints = RigidbodyConstraints2D.FreezePositionX;
+        _rb2D.gravityScale = 0.0f;
+        _jadgementBarFallPosition = Vector3.up * (Screen.height / 2.0f + _jadgementBarFallPointHeight);
+        transform.position = _jadgementBarFallPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        // 変更の可能性有
+        if(GameManager.Instance.CountDownTime < 0.0f)
+        {
+            _rb2D.gravityScale = 1.0f;
+        }
+    }
+
+
+    /// <summary>
+    /// カメラのズーム、移動に連動して JadgementBarFallPoint を Y 軸方向に移動させる
+    /// </summary>
+    /// <param name="yAxis">カメラのズームの移動方向</param>
+    public void MoveJadgementBarFallPoint(bool yAxis)
+    {
+        if (yAxis == true)
+        {
+            transform.position += Vector3.up * _jadgementBarFallPointHeight * Time.deltaTime;
+        }
+        else if (yAxis == false)
+        {
+            transform.position += Vector3.down * _jadgementBarFallPointHeight * Time.deltaTime;
+        }
+        transform.position = new Vector3(0.0f
+                                        ,Mathf.Clamp(transform.position.y
+                                                    ,_jadgementBarFallPosition.y
+                                                    ,Screen.height * 1.5f + _jadgementBarFallPointHeight)
+                                        ,0.0f);
+    }
+}
