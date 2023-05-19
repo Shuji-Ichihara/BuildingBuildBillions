9bb1af9610529c5308f3214077bcdfb2c53decbb
Shuji-Ichihara <shuji.birth-0507@outlook.jp>
2023-05-19 16:08:28 +0900

    Create JadgementBar.cs
    勝敗判定バーの当たり判定を作成した。

---
+++ BuildingBuildBillion/Assets/Member/Ichihara/Scripts/JadgementBar.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class JadgementBar : MonoBehaviour
+{
+    // 判定バーが建材と接触した時の勝敗判定 
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        JadgementBarControllerTest.Instance.Jadgement(other.gameObject.transform.position);
+    }
+}
