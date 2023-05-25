1c2c75c9e667ae3b95573d0ec9efecfd36603071
Shuji-Ichihara <shuji.birth-0507@outlook.jp>
2023-05-26 08:51:35 +0900

    Update JadgementBar.cs

--- BuildingBuildBillion/Assets/Member/Ichihara/Scripts/JadgementBar.cs
+++ BuildingBuildBillion/Assets/Member/Ichihara/Scripts/JadgementBar.cs
@@ -8,6 +8,6 @@ public class JadgementBar : MonoBehaviour
     // 判定バーが建材と接触した時の勝敗判定 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        JadgementBarControllerTest.Instance.Jadgement(other.gameObject.transform.position);
+        JadgementBarControllerTest.Instance.Jadge(other.gameObject.transform.position);
     }
 }
