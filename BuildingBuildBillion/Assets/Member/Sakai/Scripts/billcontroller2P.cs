1d78f133de6d12fa3dc694fbddfe9aa21f8b15c2
Shuji-Ichihara <shuji.birth-0507@outlook.jp>
2023-05-27 09:20:24 +0900

    Update billcontroller2P.cs
    コードを整理した。

--- BuildingBuildBillion/Assets/Member/Sakai/Scripts/billcontroller2P.cs
+++ BuildingBuildBillion/Assets/Member/Sakai/Scripts/billcontroller2P.cs
@@ -22,13 +22,12 @@ public class billcontroller2P : MonoBehaviour
     [SerializeField]
     float restTime = 0.25f;
     float fromMoveHorizonal = 0.0f;
-   
 
     void Start()
     {
 
         rb = this.gameObject.GetComponent<Rigidbody2D>();
-       
+
     }
 
     void OnCollisionEnter2D(Collision2D collision)
@@ -60,13 +59,13 @@ public class billcontroller2P : MonoBehaviour
     {
         if (Time.time - previousTime >= fallTime)
         {
-            transform.position += new Vector3(0, -1, 0);
+            transform.position += new Vector3(0, -50.0f, 0);
             previousTime = Time.time;
         }
         if (Stop == true)
         {
             rb.constraints = RigidbodyConstraints2D.None;
-            transform.position = new Vector3(transform.position.x, 1, 0);//座標を(0,1)に戻す
+            //transform.position = new Vector3(transform.position.x, 1, 0);//座標を(0,1)に戻す
             this.enabled = false;
         }
         if (billstop == true)
@@ -76,19 +75,6 @@ public class billcontroller2P : MonoBehaviour
 
             this.enabled = false;
         }
-        //Debug.Log(Stop);
-        //BillMovememt(Input.GetAxisRaw("D_Pad_H2P"), Input.GetAxisRaw("D_Pad_V2P"));
-
-        //Rotate(90);
-        //BillMovememt(Input.GetAxisRaw("Ratate_right"), Input.GetAxisRaw("Rotate_left"));
-        //if (Input.GetKeyDown("joystick button 4"))
-        //{
-        //    Debug.Log("button4");
-        //}
-        //if (Input.GetKeyDown("joystick button 5"))
-        //{
-        //    Debug.Log("button5");
-        //}
 
         fromMoveHorizonal += Time.deltaTime;
 
@@ -108,26 +94,7 @@ public class billcontroller2P : MonoBehaviour
     {
         transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), -90);
     }
-
-    //private void Rotate(InputAction.CallbackContext context, int RotateAxis, bool cannon = false)
-    //{
-    //    Debug.Log(context);
-    //    if (cannon == true)
-    //    {
-    //        if (Input.GetKey("joystick button 4"))
-    //        {
-    //            transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), RotateAxis);
-    //        }
-    //        if (Input.GetKey("joystick button 5"))
-    //        {
-    //            transform.Rotate(0, 0, -RotateAxis);
-    //        }
-    //    }
-    //    else if (cannon == false)
-    //    {
-        
-
-    //        //if (Input.GetButtonDown("Rotate_right_2P"))
+    //if (Input.GetButtonDown("Rotate_right_2P"))
     //        //{
     //        //    Debug.Log("yyye");
     //        //    transform.Rotate(0, 0, RotateAxis);
@@ -136,41 +103,41 @@ public class billcontroller2P : MonoBehaviour
     //        //{
     //        //    transform.Rotate(0, 0, -RotateAxis);
     //        //}          
-           
-            if (Input.GetKeyDown(KeyCode.LeftArrow))
-            {
-                var screenPoint = CameraControllerTest.Instance.Camera.WorldToViewportPoint(this.transform.position + new Vector3(-1, 0, 0));// 0,0~1.1
-             
-                //if (screenPoint.x >= 0.51 && screenPoint.x <= 1)
-                //    transform.position += new Vector3(-1, 0, 0);
+    /*
+     if (Input.GetKeyDown(KeyCode.LeftArrow))
+     {
+         var screenPoint = CameraControllerTest.Instance.Camera.WorldToViewportPoint(this.transform.position + new Vector3(-1, 0, 0));// 0,0~1.1
 
-                // 市原が記述
-                // 要変更
-                if (CameraControllerTest.Instance.Camera.orthographicSize < 1080.0f * 1.5f)
-                {
-                    if (screenPoint.x >= 0.55f && screenPoint.x <= 0.8f)
-                        transform.position += new Vector3(-50.0f, 0, 0);
-                }
-                if (screenPoint.x > 0.55f)
-                    transform.position += new Vector3(-50.0f, 0, 0);
-            }
-            else if (Input.GetKeyDown(KeyCode.RightArrow))
-            {
-                var screenPoint = CameraControllerTest.Instance.Camera.WorldToViewportPoint(this.transform.position + new Vector3(1, 0, 0));// 0,0~1.1
+         //if (screenPoint.x >= 0.51 && screenPoint.x <= 1)
+         //    transform.position += new Vector3(-1, 0, 0);
 
-                // 市原が記述
-                // 要変更
-                if (CameraControllerTest.Instance.Camera.orthographicSize < 1080.0f * 1.5f)
-                {
-                    if (screenPoint.x >= 0.55 && screenPoint.x <= 0.8f)
-                        transform.position += new Vector3(50.0f, 0, 0);
-                }
-                if (screenPoint.x < 1.0f)
-                    transform.position += new Vector3(50.0f, 0, 0);
-            }
-        }
-    }
+         // 市原が記述
+         // 要変更
+         if (CameraControllerTest.Instance.Camera.orthographicSize < 1080.0f * 1.5f)
+         {
+             if (screenPoint.x >= 0.55f && screenPoint.x <= 0.8f)
+                 transform.position += new Vector3(-50.0f, 0, 0);
+         }
+         if (screenPoint.x > 0.55f)
+             transform.position += new Vector3(-50.0f, 0, 0);
+     }
+     else if (Input.GetKeyDown(KeyCode.RightArrow))
+     {
+         var screenPoint = CameraControllerTest.Instance.Camera.WorldToViewportPoint(this.transform.position + new Vector3(1, 0, 0));// 0,0~1.1
 
+         // 市原が記述
+         // 要変更
+         if (CameraControllerTest.Instance.Camera.orthographicSize < 1080.0f * 1.5f)
+         {
+             if (screenPoint.x >= 0.55 && screenPoint.x <= 0.8f)
+                 transform.position += new Vector3(50.0f, 0, 0);
+         }
+         if (screenPoint.x < 1.0f)
+             transform.position += new Vector3(50.0f, 0, 0);
+     }
+ }
+}
+    */
     /// <summary>
     /// ブロック移動関数
     /// </summary>
@@ -183,74 +150,38 @@ public class billcontroller2P : MonoBehaviour
         Debug.Log(k);
         if (pad == true && k.x * k.x >= 0.25f)
         {
-            float moveDistance = 1.0f;
+            float moveDistance = 50.0f;
             if (k.x < 0) moveDistance *= -1;
-            var screenPoint = Camera.main.WorldToViewportPoint(this.transform.position+new Vector3(moveDistance, 0, 0));// 0,0~1.1
-           
-            if(screenPoint.x >= 0.51 && screenPoint.x <=1)
+            var screenPoint = Camera.main.WorldToViewportPoint(this.transform.position + new Vector3(moveDistance, 0, 0));// 0,0~1.1
+
+            if (CameraControllerTest.Instance.Camera.orthographicSize < 1080.0f * 1.5f)
+            {
+                if (screenPoint.x >= 0.55 && screenPoint.x <= 0.8)
+                    transform.position += new Vector3(moveDistance, 0, 0);
+            }
             transform.position += new Vector3(moveDistance, 0, 0);
 
             pad = false;
 
         }
-        
-        //左の壁に当たった時に値を戻す
-        //if (0.5 >= screenPoint.x)
-        //{
-        //    leftwall = true;
-        //}
-        //if (leftwall == true)
-        //{
-        //    transform.position = new Vector3(1, transform.position.y, 0);
-        //    leftwall = false;
-        //}
-        ////右の壁に当たった時に値を戻す
-        //if ( screenPoint.x>= 1)
-        //{
-        //    rightwall = true;
-        //}
-
-        //if (rightwall == true)
-        //{
-        //    transform.position = new Vector3(8, transform.position.y, 0);
-        //    rightwall = false;
-        //}
-
-        
-        
-        //Mathf.Clamp(this.transform.position.x , Camera.main.pixelWidth / 2 , Camera.main.pixelWidth);
-        //this.transform.position = new Vector3(Mathf.Clamp(this.transform.position.x, 0, Screen.width), transform.position.y, transform.position.z);
-
 
         // 自動で下に移動させつつ、下矢印キーでも移動する
         if (pad == true && k.y * k.y >= 0.25f || Input.GetKeyDown(KeyCode.DownArrow))
         {
-            if(k.y <=0.25f)
+            if (k.y <= 0.25f)
             {
-                transform.position += new Vector3(0, Mathf.Sign(k.y), 0);
+                transform.position += new Vector3(0, Mathf.Sign(k.y) * 50.0f, 0);
                 pad = false;
             }
         }
-
-        //if (Stop == true)
-        //{
-        //    rb.constraints = RigidbodyConstraints2D.None;
-        //    transform.position = new Vector3(transform.position.x, 1, 0);//新しいビルをリスポーン
-
-        //    this.enabled = false;
-            
-        //}
 
         if (billstop == true)
         {
             rb.constraints = RigidbodyConstraints2D.None;
             // transform.position = new Vector3(transform.position.x, transform.position.y, 0);//座標をその場にとどまる
             FindObjectOfType<SpownBill2P>().NewBill2P();//新しいビルをリスポーン
+        }
 
-
-        //    this.enabled = false;
-        //}
-        
     }
     void OnDisable()
     {
