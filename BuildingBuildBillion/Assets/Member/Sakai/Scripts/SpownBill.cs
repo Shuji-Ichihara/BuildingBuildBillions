365980387b9a8ac6d72990872bba24a8d3ca0522
Shuji-Ichihara <shuji.birth-0507@outlook.jp>
2023-06-01 19:47:43 +0900

    Update SpownBill
    ゲーム終了後に NewBill を実行しないようにした。

--- BuildingBuildBillion/Assets/Member/Sakai/Scripts/SpownBill.cs
+++ BuildingBuildBillion/Assets/Member/Sakai/Scripts/SpownBill.cs
@@ -1,22 +1,21 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class SpownBill : MonoBehaviour
 {
     public GameObject[] Bills;
     private GameObject obj = null;
     public GameObject Obj => obj;
+    public Vector3 BuildingPosition { get; set; }
 
     // Start is called before the first frame update
     void Start()
     {
         NewBill();
-
     }
 
     public void NewBill()
     {
+        if (GameManager.Instance.CountDownTime < 0.0f) { return; }
         if (Bills.Length != 0)
             obj = Instantiate(Bills[Random.Range(0, Bills.Length)], transform.position, Quaternion.identity);
     }
