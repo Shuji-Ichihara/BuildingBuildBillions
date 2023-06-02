eb281bdc61fc6ac015a3a463a65631227492092e
tyobo <cchobo32@gmail.com>
2023-06-02 17:54:22 +0900

    大砲を修正した

--- BuildingBuildBillion/Assets/Member/Kobayashi/Scripts/Cannon.cs
+++ BuildingBuildBillion/Assets/Member/Kobayashi/Scripts/Cannon.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Cannon : MonoBehaviour
@@ -10,44 +11,33 @@ public class Cannon : MonoBehaviour
     [SerializeField] private GameObject _cannonMuzzle;
     private Transform _cannonTransform;
     private Rigidbody2D _rg;
-    private float rotValue = 1;//仮変数
+    private float _time = 1f;
+    private bool isOnece = false;
 
     private void Start()
     {
         _rg = this.gameObject.GetComponent<Rigidbody2D>();
-        
+
     }
-    private void Update()
+    private void FixedUpdate()
     {
         _cannonTransform = this.transform;
-        Move(Input.GetAxis("D_Pad_H"), Input.GetAxis("D_Pad_V"));
+        if (_rg.IsSleeping())
+        {
+            return;
+        }
+        else
+        {
+            _time -= Time.deltaTime;
+            if (_time <= 0 && isOnece ==false)
+            {
+                Debug.Log("hoge");
+                StartCoroutine(CannonFire());
+                isOnece = true;
+            }
+        }
     }
-    private void Move(float hori, float ver)
-    {
-        if (Input.GetKey(KeyCode.D))
-        {
-            CannonRotation(-rotValue);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            CannonRotation(rotValue);
-        }
-        if (Input.GetKeyDown(KeyCode.H))
-        {
-            _rg.constraints = RigidbodyConstraints2D.None;
-            StartCoroutine(CannonFire());//こいつの呼ぶ場所は配置したらでおね。
-        }
-        if (Input.GetKeyDown("joystick button 4"))
-        {
-            Debug.Log("L"); //L?
-        }
-        if (Input.GetKeyDown("joystick button 5"))
-        {
-            Debug.Log("R");//R?
-        }
-        Debug.Log(hori);
-        Debug.Log(ver);
-    }
+
     /// <summary>
     /// 大砲の回転メソッド
     /// 引数:float RotValue
@@ -69,6 +59,7 @@ public class Cannon : MonoBehaviour
         // ローカル座標を基準に、回転を取得
         Vector3 localangle = _cannonTransform.localEulerAngles;
         //ローカル座標を基準にして_rotValue分回転
+        //少し動かさないとrigidbodyのconstraintsが解除されても自由落下しないからごく少量動かすためにある。
         localangle.z += 0.0001f;
         _cannonTransform.localEulerAngles = localangle; // 回転角度を設定
         _rg.constraints = RigidbodyConstraints2D.FreezeRotation;
@@ -76,6 +67,7 @@ public class Cannon : MonoBehaviour
         GameObject clone = Instantiate(_cannonBullet, _cannonMuzzle.transform.position, this.transform.rotation);
         clone.GetComponent<Rigidbody2D>().AddForce(_cannnonBulletImpact * _cannonMuzzle.transform.right, ForceMode2D.Impulse);
         _rg.constraints = RigidbodyConstraints2D.None;
+        this.enabled = false;
     }
 
 
