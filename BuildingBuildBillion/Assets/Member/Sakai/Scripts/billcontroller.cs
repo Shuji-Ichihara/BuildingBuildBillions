8d6ec3d0aa9b8f1883125ada9ea7d01b499ee042
taiga <jpapaxx@gmail.com>
2023-06-16 18:50:21 +0900

    当たり判定途中

--- BuildingBuildBillion/Assets/Member/Sakai/Scripts/billcontroller.cs
+++ BuildingBuildBillion/Assets/Member/Sakai/Scripts/billcontroller.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using Cysharp.Threading.Tasks;
+using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
@@ -19,7 +20,8 @@ public class billcontroller : MonoBehaviour
         Player1 = 0,
         Player2 = 1
     }
-
+    [SerializeField]
+    Collider2D Delete;
     public PlayerNum jittai;
 
     private bool _isRightPressed = false;
@@ -44,93 +46,74 @@ public class billcontroller : MonoBehaviour
     float fromMoveHorizonal = 0.0f;
     float fromRotate = 0.0f;
 
+    public float ColPoint = 50;
+    public bool ColStop = false;
+    private Vector3 screenPoint;
+
     //public Vector3 billControllerPosition { get; private set; }
 
     void Start()
     {
         rb = this.gameObject.GetComponent<Rigidbody2D>();
+        if (ColPoint == 50)
+        {
+            ColStop = true;
+        }
+        StartCoroutine(BlockStopOrDetection());
     }
 
-    void OnCollisionEnter2D(Collision2D collision)
+    //void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    if (collision.gameObject.CompareTag("stage"))
+    //    {
+    //        Stop = true;
+    //        this.GetComponent<PlayerInput>().enabled = false;
+    //    }
+    //    if (collision.gameObject.CompareTag("Bill"))
+    //    {
+
+    //        billstop = true;
+    //        this.GetComponent<PlayerInput>().enabled = false;
+    //    }
+    //}
+    private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("stage"))
         {
             Stop = true;
             this.GetComponent<PlayerInput>().enabled = false;
+            Delete.enabled = false;
         }
         if (collision.gameObject.CompareTag("Bill"))
         {
-
+            ColPoint = collision.gameObject.transform.position.x;
             billstop = true;
-            this.GetComponent<PlayerInput>().enabled = false;
-        }
-    }
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.CompareTag("stage"))
-        {
-            Stop = true;
-            this.GetComponent<PlayerInput>().enabled = false;
-        }
-        if (collision.gameObject.CompareTag("Bill"))
-        {
-
-            billstop = true;
-            this.GetComponent<PlayerInput>().enabled = false;
+            if (ColStop == false)
+            {
+                this.GetComponent<PlayerInput>().enabled = false;
+            }
+            Delete.enabled = false;
         }
         if (collision.gameObject.CompareTag("Bill2"))
         {
 
             billstop = true;
             this.GetComponent<PlayerInput>().enabled = false;
+            Delete.enabled = false;
         }
     }
 
     void Update()
     {
-        //switch (jittai)
-        //{
-        //    case PlayerNum.Player1:
-        //        restTime += Time.deltaTime;
-        //        if (_isLeftPressed)
-        //        {
-        //            Debug.Log("ho");
-        //            transform.Rotate(0, 0, 45);    //左押し処理
-        //        }
-        //        if (_isRightPressed)
-        //        {
-        //            Debug.Log("ho2");
-        //            transform.Rotate(0, 0, 45);    //右押し処理
-        //        }
 
-        //        if (_inputMove.x != 0 && restTime >= 0.25f && _isHozirontalPressed == true)
-        //        {
-        //            transform.position += new Vector3(_inputMove.x * 50, 0, 0);
-        //        }
-        //        //下長押し
-        //        transform.position += new Vector3(0, _inputMove.y * 50, 0);
-        //        break;
-        //    case PlayerNum.Player2:
-        //        restTime += Time.deltaTime;
-        //        if (_isLeftPressed)
-        //        {
-        //            Debug.Log("ho2");
-        //            transform.Rotate(0, 0, 45);    //左押し処理
-        //        }
-        //        if (_isRightPressed)
-        //        {
-        //            Debug.Log("ho222");
-        //            transform.Rotate(0, 0, 45);    //右押し処理
-        //        }
+        switch (jittai)
+        {
+            case PlayerNum.Player1:
 
-        //        if (_inputMove.x != 0 && restTime >= 0.25f && _isHozirontalPressed == true)
-        //        {
-        //            transform.position += new Vector3(_inputMove.x * 50, 0, 0);
-        //        }
-        //        //下長押し
-        //        transform.position += new Vector3(0, _inputMove.y * 50, 0);
-        //        break;
-        //}
+                break;
+            case PlayerNum.Player2:
+                break;
+        }
         fromRotate += Time.deltaTime;
 
         if (fromRotate >= RotaterestTime)
@@ -159,54 +142,35 @@ public class billcontroller : MonoBehaviour
             float moveDistance = 50.0f;
             if (_inputMove.x < 0) moveDistance *= -1;
 
-            var screenPoint = Camera.main.WorldToViewportPoint(this.transform.position + new Vector3(moveDistance, 0, 0));// 0,0~1.1
+            screenPoint = Camera.main.WorldToViewportPoint(this.transform.position + new Vector3(moveDistance, 0, 0));// 0,0~1.1
 
             if (CameraControllerTest.Instance.Camera.orthographicSize < 1080.0f * 1.5f)
-                switch (jittai)
+                if (screenPoint.x >= 0f && screenPoint.x <= 0.45f)
                 {
-                    case PlayerNum.Player1:
-                        if (screenPoint.x >= 0f && screenPoint.x <= 0.45f)
+                    if (Mathf.Ceil(_inputMove.x) == -1)
+                    {
+                        Debug.Log("RightLeft");
+                        _isHorizontalPressed = false;
+                        transform.position += new Vector3(moveDistance, 0, 0);
 
-                            if (_inputMove.x != 0)
-                            {
-                                Debug.Log("RightLeft");
-                                _isHorizontalPressed = false;
-                                transform.position += new Vector3(moveDistance, 0, 0);
+                    }
+                    if (Mathf.Ceil(_inputMove.x) == 1)
+                    {
+                        Debug.Log("RightLeft");
+                        _isHorizontalPressed = false;
+                        transform.position += new Vector3(moveDistance, 0, 0);
 
-                            }
-
-                        pad = false;
-                        break;
-                    case PlayerNum.Player2:
-                        //if (screenPoint.x >= 0.55 && screenPoint.x <= 0.8)
-                        //    if (_isLeftPressed)
-                        //    {
-                        //        Debug.Log("RightRotate");
-                        //        transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), -Rotateangle);
-                        //    }
-                        //if (_isRightPressed)
-                        //{
-                        //    Debug.Log("LeftRotate");
-                        //    transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), Rotateangle);
-                        //}
-                        if (screenPoint.x >= 0.55f && screenPoint.x <= 1f)
-
-                            if (_inputMove.x != 0)
-                            {
-                                Debug.Log("RightLeft");
-                                _isHorizontalPressed = false;
-                                transform.position += new Vector3(moveDistance, 0, 0);
-
-                            }
-
-                        pad = false;
-                        break;
+                    }
                 }
+            pad = false;
+
         }
+
         if (Time.time - previousTime >= fallTime)
         {
             transform.position += new Vector3(0, -50.0f, 0);
             previousTime = Time.time;
+
         }
         if (Stop == true)
         {
@@ -230,51 +194,12 @@ public class billcontroller : MonoBehaviour
             fromMoveHorizonal = 0.0f;
         }
 
-        //Debug.Log(k);
-        //if (pad == true && _inputMove.x * _inputMove.x >= 0.25f)
-        //{
-        //    float moveDistance = 50.0f;
-        //    if (_inputMove.x < 0) moveDistance *= -1;
-
-        //    var screenPoint2P = Camera.main.WorldToViewportPoint(this.transform.position + new Vector3(moveDistance, 0, 0));// 0,0~1.1
-
-        //    if (CameraControllerTest.Instance.Camera.orthographicSize < 1080.0f * 1.5f)
-        //    {
-        //        Debug.Log("f");
-        //        if (screenPoint2P.x >= 0.2f && screenPoint2P.x <= 0.45f)
-        //        {
-        //            if (_isLeftPressed)
-        //            {
-        //                Debug.Log("RightRotate");
-        //                transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), -Rotateangle);
-        //            }
-        //            if (_isRightPressed)
-        //            {
-        //                Debug.Log("LeftRotate");
-        //                transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), Rotateangle);
-        //            }
-        //            if (_inputMove.x != 0 && fromMoveHorizonal >= restTime && _isHorizontalPressed == true)
-        //            {
-        //                Debug.Log("RightLeft");
-        //                _isHorizontalPressed = false;
-        //                transform.position += new Vector3(moveDistance, 0, 0);
-
-        //            }
-        //        }
-        //    }
-        //    transform.position += new Vector3(moveDistance, 0, 0);
-
-        //    pad = false;
-        //}
-
-        transform.position += new Vector3(0, Mathf.Abs(_inputMove.y) *-DownSpeed, 0);
-
-
+        transform.position += new Vector3(0, Mathf.Abs(_inputMove.y) * -DownSpeed, 0);
+        
     }
 
     public void _Rotate(InputAction.CallbackContext context)
     {
-        //transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), 90);
         var y = context.control.name;
         Debug.Log(y);
         switch (context.phase)
@@ -303,42 +228,7 @@ public class billcontroller : MonoBehaviour
         _isHorizontalPressed = true;
     }
 
-    /// <summary>
-    /// ブロック移動関数
-    /// </summary>
-    /// <param name="inputhorizotal"></param>
-    /// <param name="inputvertical"></param>
-    /// <param name="Player"> false =1p true=2p</param>
-    //public void BillMovememt(InputAction.CallbackContext context)
-    //{
-    //    var k = context.ReadValue<Vector2>();
-    //    //Debug.Log(k);
-    //    if (pad == true && k.x * k.x >= 0.15f)
-    //    {
-    //        float moveDistance = 50.0f;
-    //        if (k.x < 0) moveDistance *= -1;
 
-    //        var screenPoint2P = Camera.main.WorldToViewportPoint(this.transform.position + new Vector3(moveDistance, 0, 0));// 0,0~1.1
-
-    //        if (screenPoint2P.x <= 0.48 && screenPoint2P.x >= 0)
-
-    //            transform.position += new Vector3(moveDistance, 0, 0);
-
-    //            pad = false;
-    //    }
-    //    // var screenPoint = Camera.main.WorldToViewportPoint(this.transform.position);// 0,0~1.1
-
-    //    // 自動で下に移動させつつ、下矢印キーでも移動する
-    //    if (pad == true && k.y * k.y >= 0.25f || Input.GetKeyDown(KeyCode.S))
-    //    {
-    //        if (k.y <= 0.25f)
-    //        {
-    //            transform.position += new Vector3(0, Mathf.Sign(k.y) * 50.0f, 0);
-    //            pad = false;
-    //        }
-    //    }
-
-    //}
     void OnDisable()
     {
         switch (jittai)
@@ -367,5 +257,25 @@ public class billcontroller : MonoBehaviour
     {
         Rigidbody2D rb = obj.GetComponent<Rigidbody2D>();
         rb.constraints = RigidbodyConstraints2D.FreezeAll;
+    }
+    /// <summary>
+    /// ああああああ
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator BlockStopOrDetection()
+    {
+        while(true)
+        {
+            Debug.Log("yoba");
+            //ブロックの過去のY座標を取得
+            //数秒停止
+            yield return new WaitForSeconds(1);
+            //今の座標を取得」  
+            Debug.Log("hatu");
+            //今の座標と過去の座標を比較比較的大きくして
+            //スクリプトをエナブル
+            yield return new WaitForSeconds(1);
+        }
+        
     }
 }
