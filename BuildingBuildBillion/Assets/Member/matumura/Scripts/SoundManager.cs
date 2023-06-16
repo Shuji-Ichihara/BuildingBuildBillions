551d5658fb15eefc4f93047e58f8aadc2b0f52d1
matumuratech <changzhusun8@gmail.com>
2023-06-16 15:02:40 +0900

    コンベア途中

---
+++ BuildingBuildBillion/Assets/Member/matumura/Scripts/SoundManager.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField] AudioSource bgmAudioSource;
+    [SerializeField] AudioSource seAudioSource;
+
+    [SerializeField] List<BGMSoundData> bgmSoundDatas;
+    [SerializeField] List<SESoundData> seSoundDatas;
+
+    public float masterVolume = 1;
+    public float bgmMasterVolume = 1;
+    public float seMasterVolume = 1;
+
+    public static SoundManager Instance { get; private set; }
+
+
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void PlayBGM(BGMSoundData.BGM bgm)
+    {
+        BGMSoundData data = bgmSoundDatas.Find(data => data.bgm == bgm);
+        bgmAudioSource.clip = data.audioClip;
+        bgmAudioSource.volume = data.volume * bgmMasterVolume * masterVolume;
+        bgmAudioSource.Play();
+    }
+
+
+    public void PlaySE(SESoundData.SE se)
+    {
+        SESoundData data = seSoundDatas.Find(data => data.se == se);
+        seAudioSource.volume = data.volume * seMasterVolume * masterVolume;
+        seAudioSource.PlayOneShot(data.audioClip);
+    }
+
+}
+
+[System.Serializable]
+public class BGMSoundData
+{
+    public enum BGM
+    {
+        // ここラベル、仮で名前入れてるので自由変更、可です
+        Title,//タイトル
+        Game,//ゲームシーン
+        Result,//リザルト画面
+    }
+
+    public BGM bgm;
+    public AudioClip audioClip;
+    [Range(0, 1)]
+    public float volume = 1;
+}
+
+[System.Serializable]
+public class SESoundData
+{
+    public enum SE
+    {
+        Attack,//攻撃
+        Fall,//落下
+    }
+
+    public SE se;
+    public AudioClip audioClip;
+    [Range(0, 1)]
+    public float volume = 1;
+}
+
+/*SoundManager.Instance.PlayBGM(BGMSoundData.BGM.Title);
+ 曲を流したいときに最後にTitleとかGameってかいて、適当な所にこの上の文を書いてください*/
+
