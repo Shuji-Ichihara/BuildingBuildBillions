c3185a63edc70f0909f2b1e8b100e5813b3ad776
Shuji-Ichihara <shuji.birth-0507@outlook.jp>
2023-06-16 12:03:48 +0900

    Update JadgementBar.cs
    勝敗判定を UI に反映する処理を追加した。

--- BuildingBuildBillion/Assets/Member/Ichihara/Scripts/JadgementBar.cs
+++ BuildingBuildBillion/Assets/Member/Ichihara/Scripts/JadgementBar.cs
@@ -10,7 +10,22 @@ public class JadgementBar : MonoBehaviour
     /// <param name="other">接触したオブジェクトのコライダー</param>
     private void OnCollisionEnter2D(Collision2D other)
     {
-        JadgementBarControllerTest.Instance.Jadge(other.gameObject.transform.position);
+        JadgementBarController.Instance.Jadge(other.gameObject.transform.position);
         //GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
+        GameManager.Instance.IsPreviewedResult = true;
+        if(true == other.gameObject.CompareTag("Bill"))
+        {
+            UIManager.Instance.Player1ResultText.text
+                = UIManager.Instance.YouWon;
+            UIManager.Instance.Player2ResultText.text
+                = UIManager.Instance.YouLost;
+        }
+        else if(true == other.gameObject.CompareTag("Bill2"))
+        {
+            UIManager.Instance.Player1ResultText.text
+                = UIManager.Instance.YouLost;
+            UIManager.Instance.Player2ResultText.text
+                = UIManager.Instance.YouWon;
+        }
     }
 }
