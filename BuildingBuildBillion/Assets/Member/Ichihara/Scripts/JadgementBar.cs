ef1edd8ccc28a8f5a0367863ec6401ef36563a8f
Shuji-Ichihara <shuji.birth-0507@outlook.jp>
2023-06-23 10:02:00 +0900

    Update JadgementBar.cs
    不要な記述を削除した。

--- BuildingBuildBillion/Assets/Member/Ichihara/Scripts/JadgementBar.cs
+++ BuildingBuildBillion/Assets/Member/Ichihara/Scripts/JadgementBar.cs
@@ -10,43 +10,7 @@ public class JadgementBar : MonoBehaviour
     /// <param name="other">接触したオブジェクトのコライダー</param>
     private void OnCollisionEnter2D(Collision2D other)
     {
-        Debug.Log($"aaaa");
         JadgementBarController.Instance.Objects.Add(other.gameObject);
         GameManager.Instance.IsPreviewedResult = true;
-
-        /*
-        //JadgementBarController.Instance.Jadge(other.gameObject.transform.position);
-        bool winPlayer1 = other.gameObject.CompareTag("Bill");
-        bool winPlayer2 = other.gameObject.CompareTag("Bill2");
-
-        // 引き分け
-        if (true == winPlayer1 && true == winPlayer2)
-        {
-            Debug.Log("引き分け");
-            UIManager.Instance.DrawText.fontSize = 180.0f;
-            UIManager.Instance.DrawText.text
-                = "Draw.\nThank you for Playing!";
-            UIManager.Instance.Player1ResultText.text
-                = "";
-            UIManager.Instance.Player2ResultText.text
-                = "";
-        }
-        // Player1 の勝利
-        else if (true == winPlayer1)
-        {
-            UIManager.Instance.Player1ResultText.text
-                = UIManager.Instance.YouWon;
-            UIManager.Instance.Player2ResultText.text
-                = UIManager.Instance.YouLost;
-        }
-        // Player2 の勝利
-        else if (true == winPlayer2)
-        {
-            UIManager.Instance.Player1ResultText.text
-                = UIManager.Instance.YouLost;
-            UIManager.Instance.Player2ResultText.text
-                = UIManager.Instance.YouWon;
-        }
-        */
     }
 }
