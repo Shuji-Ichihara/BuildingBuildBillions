c647facc0fa8c66b3df9f85fc65dc3d831e31bf3
Shuji-Ichihara <shuji.birth-0507@outlook.jp>
2023-08-31 21:44:42 +0900

    Update SpriteGlowEffect.cs
    フィールド変数の Range を広げた。

--- BuildingBuildBillion/Assets/SpriteGlow/Runtime/SpriteGlowEffect.cs
+++ BuildingBuildBillion/Assets/SpriteGlow/Runtime/SpriteGlowEffect.cs
@@ -59,9 +59,9 @@ namespace SpriteGlow
 
         [Tooltip("Base color of the glow.")]
         [SerializeField] private Color glowColor = Color.white;
-        [Tooltip("The brightness (power) of the glow."), Range(1, 10)]
+        [Tooltip("The brightness (power) of the glow."), Range(1, 30)]
         [SerializeField] private float glowBrightness = 2f;
-        [Tooltip("Width of the outline, in texels."), Range(0,10)]
+        [Tooltip("Width of the outline, in texels."), Range(0,30)]
         [SerializeField] private int outlineWidth = 1;
         [Tooltip("Threshold to determine sprite borders."), Range(0f, 1f)]
         [SerializeField] private float alphaThreshold = .01f;
