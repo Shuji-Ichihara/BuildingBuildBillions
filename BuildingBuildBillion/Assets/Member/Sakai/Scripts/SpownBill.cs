f4e5d3c03fa2b40ff1c21f189905093757348c8f
Shuji-Ichihara <shuji.birth-0507@outlook.jp>
2023-08-31 21:49:32 +0900

    Update SpownBill
    次建材表示が完成した。

--- BuildingBuildBillion/Assets/Member/Sakai/Scripts/SpownBill.cs
+++ BuildingBuildBillion/Assets/Member/Sakai/Scripts/SpownBill.cs
@@ -21,18 +21,12 @@ public class SpownBill : MonoBehaviour
         {
             int indexToUse = randomIndices[0];
             GameObject newBill = Instantiate(Bills[indexToUse], transform.position, Quaternion.identity);
-            NewBuildingcon newBuildingcon = newBill.GetComponent<NewBuildingcon>();
-            if (newBuildingcon.sprites.Length > 0)
-            {
-                // 最初にスプライトをランダムに選択する
-                SpriteRenderer spriteRenderer = newBuildingcon.GetComponent<SpriteRenderer>();
-                newBuildingcon.ChangeSpriteRandomly(spriteRenderer);
-            }
             GameManager.Instance.Obj = newBill;
             UIManager.Instance.Player1NextBuildingMaterial.sprite = UIManager.Instance.PreviewBuildingSprite(Bills[randomIndices[1]]);
 
             randomIndices.RemoveAt(0); // 配列の先頭の値を削除
             int newIndex = GenerateRandomIndex(); // 新しい乱数を生成
+            SetSpriteInBills(newIndex);
             randomIndices.Add(newIndex); // 新しい乱数を配列の最後尾に追加
         }
     }
@@ -42,6 +36,7 @@ public class SpownBill : MonoBehaviour
         for (int i = 0; i < count; i++)
         {
             int newIndex = GenerateRandomIndex();
+            SetSpriteInBills(newIndex);
             randomIndices.Add(newIndex);
         }
     }
@@ -49,5 +44,16 @@ public class SpownBill : MonoBehaviour
     private int GenerateRandomIndex()
     {
         return Random.Range(0, Bills.Length);
+    }
+
+    private void SetSpriteInBills(int index)
+    {
+        NewBuildingcon newBuildingcon = Bills[index].GetComponent<NewBuildingcon>();
+        if (newBuildingcon.sprites.Length > 0)
+        {
+            // 最初にスプライトをランダムに選択する
+            SpriteRenderer spriteRenderer = newBuildingcon.GetComponent<SpriteRenderer>();
+            newBuildingcon.ChangeSpriteRandomly(spriteRenderer);
+        }
     }
 }
--- BuildingBuildBillion/Assets/Member/Sakai/Scripts/SpownBill2P.cs
+++ BuildingBuildBillion/Assets/Member/Sakai/Scripts/SpownBill2P.cs
@@ -21,18 +21,12 @@ public class SpownBill2P : MonoBehaviour
         {
             int indexToUse = randomIndices2P[0];
             GameObject newBill2P = Instantiate(Bills2P[indexToUse], transform.position, Quaternion.identity);
-            NewBuildingcon newBuildingcon = newBill2P.GetComponent<NewBuildingcon>();
-            if (newBuildingcon.sprites.Length > 0)
-            {
-                // 最初にスプライトをランダムに選択する
-                SpriteRenderer spriteRenderer = newBuildingcon.GetComponent<SpriteRenderer>();
-                newBuildingcon.ChangeSpriteRandomly(spriteRenderer);
-            }
             GameManager.Instance.Obj2 = newBill2P;
-            UIManager.Instance.Player1NextBuildingMaterial.sprite = UIManager.Instance.PreviewBuildingSprite(Bills2P[randomIndices2P[1]]);
+            UIManager.Instance.Player2NextBuildingMaterial.sprite = UIManager.Instance.PreviewBuildingSprite(Bills2P[randomIndices2P[1]]);
 
             randomIndices2P.RemoveAt(0); // 配列の先頭の値を削除
             int newIndex = GenerateRandomIndex2P(); // 新しい乱数を生成
+            SetSpriteInBills2P(newIndex);
             randomIndices2P.Add(newIndex); // 新しい乱数を配列の最後尾に追加
         }
     }
@@ -42,6 +36,7 @@ public class SpownBill2P : MonoBehaviour
         for (int i = 0; i < count; i++)
         {
             int newIndex = GenerateRandomIndex2P();
+            SetSpriteInBills2P(newIndex);
             randomIndices2P.Add(newIndex);
         }
     }
@@ -49,5 +44,16 @@ public class SpownBill2P : MonoBehaviour
     private int GenerateRandomIndex2P()
     {
         return Random.Range(0, Bills2P.Length);
+    }
+
+    private void SetSpriteInBills2P(int index)
+    {
+        NewBuildingcon newBuildingcon = Bills2P[index].GetComponent<NewBuildingcon>();
+        if (newBuildingcon.sprites.Length > 0)
+        {
+            // 最初にスプライトをランダムに選択する
+            SpriteRenderer spriteRenderer = newBuildingcon.GetComponent<SpriteRenderer>();
+            newBuildingcon.ChangeSpriteRandomly(spriteRenderer);
+        }
     }
 }
