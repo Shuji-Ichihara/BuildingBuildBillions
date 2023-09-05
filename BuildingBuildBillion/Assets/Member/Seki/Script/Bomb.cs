a57186c991b665ec25967e05c6995a33466d35af
sekiRyoma <ryo-ma0110@docomo.ne.jp>
2023-09-05 23:56:55 +0900

    ポートフォリオ用手直し

--- BuildingBuildBillion/Assets/Member/Seki/Script/Bomb.cs
+++ BuildingBuildBillion/Assets/Member/Seki/Script/Bomb.cs
@@ -9,6 +9,7 @@ public class Bomb : MonoBehaviour
     CircleCollider2D _bombCollider;
     Rigidbody2D _rb;
     Animator _animator;
+
     [SerializeField]
     GameObject colObj;
     [SerializeField, Header("ボムの爆発の範囲")]
@@ -17,6 +18,7 @@ public class Bomb : MonoBehaviour
     private int bombSpeed = 0;
     [SerializeField,Header("ボムの吹っ飛ばすパワー")]
     private int power=0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,69 +27,51 @@ public class Bomb : MonoBehaviour
         _animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        /*
-        if(Input.GetKeyDown(KeyCode.A))
-        {
-            //Debug.Log(this.transform.localPosition);
-            //BombEvent();
-        }*/
-    }
 
     void BombEvent()
-    {//ここで爆発
+    {//アニメーションから呼び出し
+
         StartCoroutine(BombCor());
-        //_bombCollider.radius = 5;
         GetComponent<SpriteRenderer>().enabled = false;
-        //_bombCollider.enabled = false;
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        
-        //_animator.SetBool("BombBool", true);
-        
-    }
-    
+
     public void AnimStart()
     {
         _animator.SetBool("BombBool", true);
     }
 
-
     private void OnTriggerEnter2D(Collider2D col)
-    {//コライだー通り抜けたら飛ばす
-        //ver2
+    {//コライダー通り抜けたら飛ばす
+
         Vector3 hit;
         hit = col.ClosestPoint(this.transform.position);//trigger衝突位置
 
-
         hit = transform.InverseTransformPoint(hit);//触れたオブジェクトのローカル座標
-        //Debug.Log(hit);
-        //Debug.LogError(Mathf.Atan2(hit.y,hit.x)*Mathf.Rad2Deg);//角度だよ
 
         col.gameObject.GetComponent<Rigidbody2D>().AddForce(hit*power, ForceMode2D.Impulse);
-        //吹っ飛ばす
+        //吹っ飛ばす処理
     }
 
     IEnumerator BombCor() 
-    {
-        //爆発効果音入れるならココ!!
+    {//爆発の一連の処理
 
         _bombCollider.enabled = false;
         _bombCollider.enabled = true;
         _bombCollider.isTrigger = true;
+
         float count = 0;
         float time = 1f / bombSpeed;
+
         _rb.bodyType = RigidbodyType2D.Static;
+
         while(count<time)
         {
             _bombCollider.radius += Time.deltaTime*bombSpeed*bombRadius;
             count+=Time.deltaTime;
             yield return null;
         }
+
         _bombCollider.enabled = false;
     }
 }
